1e7c7c9b30d152a2e1189309a5f32bec4889be36
ISNewton <ashrfalhag.11@gmail.com>
2023-11-04 16:40:55 +0200

    fix icons reveal animation

--- components/MainContent.vue
+++ components/MainContent.vue
@@ -7,31 +7,40 @@
           Ashraf Alhaj - Creative Fullstack Engineer
         </h1>
 
-        <h2 
-          v-motion
-          :initial="{ opacity: 0, y: 100 }" 
-          :enter="{ opacity: 1, y: 0, scale: 1 }" 
-          :delay="500"
+        <h2 v-motion :initial="{ opacity: 0, y: 100 }" :enter="{ opacity: 1, y: 0, scale: 1 }" :delay="500"
           class="text-black opacity-0 mt-8 font-mono">
           Hi there! I'm Ashraf Alhaj from Khartoum, Sudan, and I like to make
           things. I'm currently building Airlume and RocketLog, and I work on
           the Laravel core team.
         </h2>
         <div class="flex justify-center pt-12 text-center gap-20">
-          <div ref="reference"
-            class="w-8 hover:w-12 transition-all ease-in-out duration-500 h-8 laravel-shadow cursor-pointer">
-            <img class="max-w-full" src="images/laravel.svg" />
+          <div class="opacity-0" v-motion :initial="{ opacity: 0, y: 100 }" :enter="{ opacity: 1, y: 0 }" :delay="700">
+            <div
+              class="w-8 transform hover:scale-125 transition-all ease-in-out duration-500 laravel-shadow cursor-pointer">
+              <img class="max-w-full" src="images/laravel.svg" />
+            </div>
           </div>
 
-          <div class="w-8 hover:w-12 transition-all ease-in-out duration-500 h-8 vue-shadow cursor-pointer">
-            <img class="max-w-full" src="images/vue.svg" />
-          </div>
-          <div class="w-8 hover:w-12 transition-all ease-in-out duration-500 livewire-shadow cursor-pointer">
-            <img class="max-w-full" src="images/livewire.svg" />
+          <div class="opacity-0" v-motion :initial="{ opacity: 0, y: 100 }" :enter="{ opacity: 1, y: 0, scale: 1 }"
+            :delay="800">
+            <div
+              class="w-8 transform hover:scale-125 transition-all ease-in-out duration-500 h-8 vue-shadow cursor-pointer">
+              <img class="max-w-full" src="images/vue.svg" />
+            </div>
           </div>
 
-          <div class="w-8 ease-in transition-all hover:w-12 duration-500 react-shadow cursor-pointer">
-            <img class="max-w-full" src="images/react.svg" />
+          <div v-motion :initial="{ opacity: 0, y: 100 }" :enter="{ opacity: 1, y: 0 }" :delay="900" class="opacity-0">
+            <div
+              class="w-8 transform hover:scale-125 transition-all ease-in-out duration-500 livewire-shadow cursor-pointer">
+              <img class="max-w-full" src="images/livewire.svg" />
+            </div>
+          </div>
+
+          <div v-motion :initial="{ opacity: 0, y: 100 }" :enter="{ opacity: 1, y: 0, scale: 1 }" :delay="1000"
+            class="opacity-0">
+            <div class="w-8 ease-in transition-all transform hover:scale-125 duration-500 react-shadow cursor-pointer">
+              <img class="max-w-full" src="images/react.svg" />
+            </div>
           </div>
         </div>
       </div>
